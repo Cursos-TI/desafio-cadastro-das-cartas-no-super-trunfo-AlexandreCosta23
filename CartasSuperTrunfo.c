184713425c73900adb88b03f92342a1940515b3b
AlexandreCosta23 <102429132+AlexandreCosta23@users.noreply.github.com>
2025-07-29 15:42:47 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -29,9 +29,9 @@ int main() {
     printf(" Por favor insira a área da cidade em quilomêtros quadrados: \n");
     scanf("%f", &area_1);
     printf(" Por favor insira o Produto Interno Bruto (PIB): \n");
-    scanf("%f", &npontoturistico_1);
+    scanf("%f", &pib_1);
     printf(" Por favor insira a quantidade de pontos turisticos: \n");
-    scanf("%d", estado_1);
+    scanf("%d", &npontoturistico_1);
 
     // O bloco abaixo é para recebimento dos dados da carta 2.
 
@@ -47,13 +47,32 @@ int main() {
     printf(" Por favor insira a área da cidade em quilomêtros quadrados: \n");
     scanf("%f", &area_2);
     printf(" Por favor insira o Produto Interno Bruto (PIB): \n");
-    scanf("%f", &npontoturistico_2);
+    scanf("%f", &pib_2);
     printf(" Por favor insira a quantidade de pontos turisticos: \n");
-    scanf("%d", estado_2);
+    scanf("%d", &npontoturistico_2);
+
+    //A seguir o bloco abaixo irá informar os dados da carta 1.
+
+    printf("Carta 1 \n");
+    printf("Estado: %s \n", estado_1);
+    printf("Código: %s \n", cod_carta_1);
+    printf("Nome da Cidade: %s \n", nomecidade_1);
+    printf("População: %d \n", populacao_1);
+    printf("Área: %f \n", area_1);
+    printf("PIB: %f \n", pib_1);
+    printf("Número de pontos turísticos: %d \n", npontoturistico_1);
+
+    //A seguir o bloco abaixo irá informar os dados da carta 2.
+
+    printf("Carta 2 \n");
+    printf("Estado: %s \n", estado_2);
+    printf("Código: %s \n", cod_carta_2);
+    printf("Nome da Cidade: %s \n", nomecidade_2);
+    printf("População: %d \n", populacao_2);
+    printf("Área: %f \n", area_2);
+    printf("PIB: %f \n", pib_2);
+    printf("Número de pontos turísticos: %d \n", npontoturistico_2);
 
     
-
-    
-
     return 0;
 }
